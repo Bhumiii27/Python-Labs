1da8461f32faaad4b5058d506b69a2f50279a0ff
Bhumi Solanki <bhumisolanki672@gmail.com>
2025-08-29 12:04:12 +0530

    Create Lab1.py

---
+++ Lab1.py
@@ -0,0 +1,16 @@
+# --------- Post Lab Exercises ---------
+
+# 1. Display message 5 times
+for i in range(5):
+    print("Welcome to Python")
+
+# 2. Display a table (Example: Numbers and Squares)
+print("\nNumber\tSquare")
+for i in range(1, 6):
+    print(i, "\t", i**2)
+
+# 3. Display some results
+numerator = (9.5 * 4.5) - (2.5 * 3)
+denominator = 45.5 - 3.5
+result = numerator/denominator
+print("Result:", result)
