b74f5db38cb283b598e96c8ffacda9a416bbf490
Bhumi Solanki <bhumisolanki672@gmail.com>
2025-11-23 23:58:57 +0530

    Added Lab27.py

---
+++ Lab27.py
@@ -0,0 +1,11 @@
+#1.
+import pandas as pd
+import seaborn as sns
+ 
+# initialise data of lists
+data = {'Name':[ 'Mohe' , 'Karnal' , 'Yrik' , 'jack' ],
+        'Age':[ 30 , 21 , 29 , 28 ]}
+df = pd.DataFrame( data )
+sns.boxplot( data['Age'] )
+#2.
+sns.violinplot(data['Age'])
