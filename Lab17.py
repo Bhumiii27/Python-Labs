7e68b9b86898d9972aa280258ccbb09b39ea21fd
Bhumi Solanki <bhumisolanki672@gmail.com>
2025-11-23 23:05:59 +0530

    Added Lab17.py

---
+++ Lab17.py
@@ -0,0 +1,13 @@
+print("Post Lab")
+print("Question 1")
+n, z = sp.symbols('n z')
+x_n = 3**n
+X_z = sp.summation(x_n * z**(-n), (n, 0, sp.oo))
+print("Z-Transform of x[n] = 3^n u[n]:")
+sp.pprint(X_z, use_unicode=True)
+print("\nQuestion 2")
+n, z, omega = sp.symbols('n z omega')
+x_n = sp.cos(omega * n)
+X_z = sp.summation(x_n * z**(-n), (n, 0, sp.oo))
+print("Z-Transform of x[n] = cos(ωn) u[n]:")
+sp.pprint(X_z, use_unicode=True)
